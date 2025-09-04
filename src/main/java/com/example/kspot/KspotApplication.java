75d0ce83957a41609ec53b0c92c5d22773cadead
DESKTOP-DHJI2MJ\user <choroc2020@gmail.com>
2025-09-04 20:58:36 +0900

    feat: DB-EC2연동 및 간단한 테스트 수행

---
+++ src/main/java/com/example/kspot/KspotApplication.java
@@ -0,0 +1,13 @@
+package com.example.kspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KspotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KspotApplication.class, args);
+  }
+
+}
