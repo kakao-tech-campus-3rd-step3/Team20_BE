42ed0d9cfc79dca54eeb5c1026856c26d59ba891
DESKTOP-DHJI2MJ\user <choroc2020@gmail.com>
2025-09-08 19:03:19 +0900

    test: DB 접속 및 데이터 검증 로직 보완

--- src/test/java/com/example/kspot/KspotApplicationTests.java
+++ src/test/java/com/example/kspot/KspotApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.kspot;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -14,18 +17,45 @@ class KspotApplicationTests {
   private JdbcTemplate jdbcTemplate;
 
   @Test
-  void printAllTables() {
-    String[] tables = {"locations", "contents"};
+  void checkContentsTable(){
+    String sql = "SELECT content_id, category, title, poster_image_url, release_date FROM contents";
+    List<Map<String, Object>> results = jdbcTemplate.queryForList(sql);
 
-    for (String table : tables) {
-      System.out.println("=== Table: " + table + " ===");
-      List<Map<String, Object>> rows = jdbcTemplate.queryForList("SELECT * FROM " + table);
+    assertThat(results).isNotEmpty();
 
-      for (Map<String, Object> row : rows) {
-        System.out.println(row);
-      }
+    boolean contents = results.stream()
+        .anyMatch(row -> row.get("content_id").toString().equals("7")
+        && "기생충".equals(row.get("title"))
+        && "2019-05-30".equals(row.get("release_date").toString()));
+    assertThat(contents).isTrue();
+  }
 
-      System.out.println(); // 줄바꿈
-    }
+  @Test
+  void checkArtistsTable(){
+    String sql = "SELECT artist_id, name, profile_image_url FROM artists";
+    List<Map<String, Object>> results = jdbcTemplate.queryForList(sql);
+
+    assertThat(results).isNotEmpty();
+
+    boolean contents = results.stream()
+        .anyMatch(row -> row.get("artist_id").toString().equals("1")
+            && "방탄소년단".equals(row.get("name")));
+    assertThat(contents).isTrue();
+  }
+
+  @Test
+  void checkLocationsTable(){
+    String sql = "SELECT location_id, name, address, latitude, longitude FROM locations";
+    List<Map<String, Object>> results = jdbcTemplate.queryForList(sql);
+
+    assertThat(results).isNotEmpty();
+
+    boolean contents = results.stream()
+        .anyMatch(row -> row.get("location_id").toString().equals("3")
+            && "경복궁".equals(row.get("name"))
+            && "서울특별시 종로구 사직로 161".equals(row.get("address"))
+            && new BigDecimal("37.57961700").compareTo((BigDecimal) row.get("latitude")) == 0
+            && new BigDecimal("126.97704100").compareTo((BigDecimal) row.get("longitude")) == 0); // 숫자 비교를 위해 BigDecimal 사용
+    assertThat(contents).isTrue();
   }
 }
