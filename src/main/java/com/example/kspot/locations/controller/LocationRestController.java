95d3e6970f3276e4f7b04ad66170842b5e1d99d0
LMH <lmh10087215@gmail.com>
2025-09-19 16:35:54 +0900

    feat : LocationService 추가

--- src/main/java/com/example/kspot/locations/controller/LocationRestController.java
+++ src/main/java/com/example/kspot/locations/controller/LocationRestController.java
@@ -1,6 +1,7 @@
 package com.example.kspot.locations.controller;
 
 import com.example.kspot.locations.dto.LocationResponse;
+import com.example.kspot.locations.service.LocationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
