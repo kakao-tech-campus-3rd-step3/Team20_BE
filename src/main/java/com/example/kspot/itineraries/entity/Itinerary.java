f919cb6511bcc64ae04a15d691a1be6388fe0b38
DESKTOP-DHJI2MJ\user <choroc2020@gmail.com>
2025-09-25 16:38:46 +0900

    feat: itinerary와 itineraryLocation Entity간 관계정립

--- src/main/java/com/example/kspot/itineraries/entity/Itinerary.java
+++ src/main/java/com/example/kspot/itineraries/entity/Itinerary.java
@@ -1,15 +1,23 @@
 package com.example.kspot.itineraries.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "itineraries")
+@Getter
+@Setter
 public class Itinerary {
 
   @Id
@@ -29,4 +37,19 @@ public class Itinerary {
 
   @Column(name = "created_at", updatable = false, insertable = false)
   private LocalDateTime created_at;
+
+  @OneToMany(mappedBy = "ininerary", cascade = CascadeType.ALL)
+  private List<ItineraryLocation> itineraryLocations = new ArrayList<>();
+
+  // 동선에 장소 추가
+  public void addItineraryLocation(ItineraryLocation itineraryLocation) {
+    itineraryLocations.add(itineraryLocation);
+    itineraryLocation.setItinerary(this);
+  }
+
+  // 동선에 장소 삭제
+  public void removeItineraryLocation(ItineraryLocation itineraryLocation) {
+    itineraryLocations.remove(itineraryLocation);
+    itineraryLocation.setItinerary(null);
+  }
 }
