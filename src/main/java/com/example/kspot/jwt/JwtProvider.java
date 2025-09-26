93846cfcfc5b56002186a07518420dc6e82fafb1
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-09-26 18:01:06 +0900

    feat: jwt생성기 작성

---
+++ src/main/java/com/example/kspot/jwt/JwtProvider.java
@@ -0,0 +1,34 @@
+package com.example.kspot.jwt;
+
+import com.example.kspot.users.entity.Users;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+    private final String secretKey = "kspot의시크릿키입니다.";
+
+    public String generateToken(Users user) {
+        return Jwts.builder()
+                .setSubject(user.getUserId().toString())
+                .claim("name", user.getEmail())
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+    }
+
+    public Long validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return Long.parseLong(claims.getSubject());
+        } catch (Exception e) {
+            throw new RuntimeException("토큰 유효성 검사 실패");
+        }
+    }
+
+}
