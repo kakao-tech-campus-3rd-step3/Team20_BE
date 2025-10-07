c6252341c3e295d8de60f42238a3f6e7ba8bff5e
LMH <lmh10087215@gmail.com>
2025-10-07 15:41:27 +0900

    docs: Swagger UI에 JWT 인증 설정 추가

--- src/main/java/com/example/kspot/config/SwaggerConfig.java
+++ src/main/java/com/example/kspot/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.example.kspot.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +14,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI kspotOpenAPI() {
+        //JWT Security 설정
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("K-SPOT API 명세서")
                         .description("K-SPOT API 문서입니다.")
                         .version("v1.0.0")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement);
     }
 }
