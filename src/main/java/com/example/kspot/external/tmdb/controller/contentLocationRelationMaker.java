de45aa370a231b3a568331e6aed5827ed1f5b32a
DESKTOP-DHJI2MJ\user <choroc2020@gmail.com>
2025-10-20 19:39:04 +0900

    fix: 관계 생성Controller 응답 string -> DTO로 수정

--- src/main/java/com/example/kspot/external/tmdb/controller/contentLocationRelationMaker.java
+++ src/main/java/com/example/kspot/external/tmdb/controller/contentLocationRelationMaker.java
@@ -1,15 +1,18 @@
 package com.example.kspot.external.tmdb.controller;
 
+import com.example.kspot.external.tmdb.service.contentLoctionService;
+import com.example.kspot.global.dto.ApiResponseDto;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.kspot.external.tmdb.service.contentLoctionService;
+
 @RestController
 public class contentLocationRelationMaker {
+
   private final contentLoctionService service;
 
   public contentLocationRelationMaker(contentLoctionService service) {
@@ -17,13 +20,15 @@ public class contentLocationRelationMaker {
   }
 
   @PostMapping("/api/content/location/relationMake")
-  public String relationMake() throws Exception {
-    // resources 폴더 안 파일 접근
+  public ResponseEntity<ApiResponseDto<String>> relationMake() throws Exception {
     ClassPathResource resource = new ClassPathResource("한국문화정보원_미디어콘텐츠_UTF8.csv");
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream(), Charset.forName("UTF-8")));
+    BufferedReader br = new BufferedReader(
+        new InputStreamReader(resource.getInputStream(), Charset.forName("UTF-8")));
 
-    service.importFromCsv(br); // importFromCsv 메서드를 BufferedReader 버전으로 오버로딩
-    return "content_location간 관계 테이블 생성완료!";
+    service.importFromCsv(br);
+    ApiResponseDto<String> response =
+        new ApiResponseDto<>(200, "content_location간 관계 테이블 생성완료!", null);
+    return ResponseEntity.ok(response);
   }
 }
