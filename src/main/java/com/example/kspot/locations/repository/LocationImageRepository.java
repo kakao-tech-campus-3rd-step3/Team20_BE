51ceb8958bd5dd37c5fc328a211370a92563be94
Lmmhhhh <lmh10087215@gmail.com>
2025-10-31 03:51:21 +0900

    feat: Location 응답에 장소 설명, 관련 이미지 추가되도록 서비스 수정

--- src/main/java/com/example/kspot/locations/repository/LocationImageRepository.java
+++ src/main/java/com/example/kspot/locations/repository/LocationImageRepository.java
@@ -4,7 +4,9 @@ import com.example.kspot.locations.entity.LocationImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LocationImageRepository extends JpaRepository<LocationImage, Long> {
-
+    List<LocationImage> findByLocation_LocationId(Long locationId);
 }
